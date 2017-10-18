1b40643ee00007d9f29f0dbd76cf9596f75edb99
Gregory Goijaerts <crecketgaming@gmail.com>
2017-10-19 00:18:40 +0200

    Prettier main menu and added icon to main app folder

--- src/react/Components/OptionsDrawer.jsx
+++ src/react/Components/OptionsDrawer.jsx
@@ -1,29 +1,52 @@
 import React from "react";
+import { connect } from "react-redux";
 import { withTheme } from "material-ui/styles";
 import Drawer from "material-ui/Drawer";
-import List, { ListItem, ListSubheader } from "material-ui/List";
-import { connect } from "react-redux";
+import List, {
+    ListItem,
+    ListSubheader,
+    ListItemText,
+    ListItemIcon
+} from "material-ui/List";
+import Input, { InputLabel } from "material-ui/Input";
+import { MenuItem } from "material-ui/Menu";
+import { FormControl } from "material-ui/Form";
+import Select from "material-ui/Select";
+import Avatar from "material-ui/Avatar";
+import PowerSettingsIcon from "material-ui-icons/PowerSettingsNew";
+
 import { setTheme } from "../Actions/theme";
 import { closeDrawer } from "../Actions/options_drawer";
 
-import Input, { InputLabel } from "material-ui/Input";
-import { MenuItem } from "material-ui/Menu";
-import { FormControl, FormHelperText } from "material-ui/Form";
-import Select from "material-ui/Select";
-
 const styles = {
     list: {
-        width: 250
+        width: 250,
+        display: "flex",
+        flexDirection: "column"
     },
     listItem: {
         paddingTop: 0,
         paddingBottom: 0
+    },
+    listFiller: {
+        flex: "1 1 100%"
+    },
+    listBottomItem: {
+        flex: 0
     },
     formControl: {
         width: "100%"
     },
     selectField: {
         width: "100%"
+    },
+    avatar: {
+        width: 50,
+        height: 50
+    },
+    bunqLink: {
+        color: "inherit",
+        textDecoration: "none"
     }
 };
 
@@ -43,13 +66,37 @@ class OptionsDrawer extends React.Component {
         this.props.setTheme(event.target.value);
     };
 
+    closeApp() {
+        window.close();
+    }
+
     render() {
         const { theme, open } = this.props;
 
         const drawerList = (
             <List style={styles.list}>
-                <ListSubheader>Options</ListSubheader>
+                <a
+                    className="js-external-link"
+                    style={styles.bunqLink}
+                    href="https://github.com/BunqCommunity/BunqDesktop"
+                    target="_blank"
+                >
+                    <ListItem button onClick={this.openGithub}>
+                        <Avatar style={styles.avatar} onClick={this.openGithub}>
+                            <img
+                                src="./images/512x512.png"
+                                style={styles.avatar}
+                            />
+                        </Avatar>
+                        <ListItemText
+                            onClick={this.openGithub}
+                            primary="BunqDesktop"
+                            secondary={`Version ${CURRENT_VERSION}`}
+                        />
+                    </ListItem>
+                </a>
 
+                <ListSubheader>Options</ListSubheader>
                 <ListItem>
                     <FormControl style={styles.formControl}>
                         <InputLabel htmlFor="theme-selection">Theme</InputLabel>
@@ -65,12 +112,24 @@ class OptionsDrawer extends React.Component {
                         </Select>
                     </FormControl>
                 </ListItem>
+
+                <ListItem style={styles.listFiller} />
+                <ListItem
+                    button
+                    style={styles.listBottomItem}
+                    onClick={this.closeApp}
+                >
+                    <ListItemIcon>
+                        <PowerSettingsIcon />
+                    </ListItemIcon>
+                    Quit BunqDesktop
+                </ListItem>
             </List>
         );
 
         return [
             <Drawer
-                open={open}
+                open={true}
                 onRequestClose={this.props.closeDrawer}
                 anchor="left"
             >
