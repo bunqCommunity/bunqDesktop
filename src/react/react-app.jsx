d0ee5b66a4ed9043fa89e30176ff7481b1cd3a9a
Dennis Snijder <dennissnijder97@gmail.com>
2017-10-21 23:54:08 +0200

    Added React-GA

--- src/react/react-app.jsx
+++ src/react/react-app.jsx
@@ -4,12 +4,16 @@ import store from "store";
 import BunqJSClient from "@bunq-community/bunq-js-client";
 import App from "./App";
 
+import ReactGA from 'react-ga'
+
 require("../scss/main.scss");
 
 // create a new bunq js client and inject into the app
 const BunqJSClientInstance = new BunqJSClient(store);
 
+ReactGA.initialize('UA-87358128-5');
+
 ReactDOM.render(
-    <App BunqJSClient={BunqJSClientInstance} />,
+    <App ReactGA={ReactGA} BunqJSClient={BunqJSClientInstance} />,
     document.getElementById("app")
 );
