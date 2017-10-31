b7f9a50a17c7829395fee329b6c11d54c8f6c69c
Gregory Goijaerts <crecketgaming@gmail.com>
2017-10-31 22:18:07 +0100

    Trigger a Version event  for analytics

--- src/react/react-app.jsx
+++ src/react/react-app.jsx
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom";
 import store from "store";
 import BunqJSClient from "@bunq-community/bunq-js-client";
 import App from "./App";
-import { generateGUID } from './Helpers/Utils';
+import { generateGUID } from "./Helpers/Utils";
 
-import analytics from 'universal-analytics'
+import analytics from "universal-analytics";
 
 require("../scss/main.scss");
 
-let userGUID = store.get('user-guid');
+let userGUID = store.get("user-guid");
 
 if (!userGUID) {
     userGUID = generateGUID();
-    store.set('user-guid', userGUID);
+    store.set("user-guid", userGUID);
 }
 
 // create a new bunq js client and inject into the app
@@ -22,6 +22,8 @@ const analyticsInstance = analytics("UA-87358128-5", userGUID, {
     strictCidFormat: false
 });
 
+// send the Version event to track the user's version
+analyticsInstance.event("Version", process.env.CURRENT_VERSION).send();
 
 ReactDOM.render(
     <App analytics={analyticsInstance} BunqJSClient={BunqJSClientInstance} />,
