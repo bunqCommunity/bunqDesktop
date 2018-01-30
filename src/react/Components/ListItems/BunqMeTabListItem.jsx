241813ca32a10090fa60d4831039ea6e91bf3a2d
Gregory Goijaerts <crecketgaming@gmail.com>
2018-01-30 12:03:48 +0100

    Added payment overview to bunqme items

--- src/react/Components/ListItems/BunqMeTabListItem.jsx
+++ src/react/Components/ListItems/BunqMeTabListItem.jsx
@@ -15,11 +15,13 @@ import Avatar from "material-ui/Avatar";
 import CopyIcon from "material-ui-icons/ContentCopy";
 import Share from "material-ui-icons/Share";
 
+import PaymentListItem from "./PaymentListItem";
 import { humanReadableDate, formatMoney } from "../../Helpers/Utils";
 
 const styles = {
     actionListItem: {
-        padding: 16
+        padding: 16,
+        marginTop: 16
     },
     smallAvatar: {
         width: 50,
@@ -31,12 +33,17 @@ class BunqMeTabListItem extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            extraInfoOpen: false
+            extraInfoOpen: false,
+            paymentsOpen: false
         };
     }
 
     toggleExtraInfo = () => {
         this.setState({ extraInfoOpen: !this.state.extraInfoOpen });
+    };
+
+    togglePayments = () => {
+        this.setState({ paymentsOpen: !this.state.paymentsOpen });
     };
 
     cancelTab = () => {
@@ -80,6 +87,8 @@ class BunqMeTabListItem extends React.Component {
             .filter(merchant => merchant.available)
             .map(merchant => merchant.merchant_type)
             .join(", ");
+
+        const bunqMeTabPayments = bunqMeTab.result_inquiries;
 
         return [
             <ListItem button onClick={this.toggleExtraInfo}>
@@ -126,12 +135,22 @@ class BunqMeTabListItem extends React.Component {
                     />
                 </ListItem>
 
-                <ListItem dense>
+                <ListItem button dense onClick={this.togglePayments}>
                     <ListItemText
                         primary="Number of payments"
                         secondary={"" + numberOfPayments}
                     />
                 </ListItem>
+                <Collapse in={this.state.paymentsOpen} unmountOnExit>
+                    {bunqMeTabPayments.map(bunqMeTabPayment => {
+                        return (
+                            <PaymentListItem
+                                payment={bunqMeTabPayment.payment.Payment}
+                                BunqJSClient={this.props.BunqJSClient}
+                            />
+                        );
+                    })}
+                </Collapse>
 
                 <ListItem style={styles.actionListItem}>
                     <ListItemSecondaryAction>
