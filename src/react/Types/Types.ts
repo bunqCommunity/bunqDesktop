c41d8ea3fcf41080e5423b9f7f6f9b4d3a9d69b0
Gregory Goijaerts <crecketgaming@gmail.com>
2018-03-27 16:21:25 +0200

    Add more detailed types, payment model and implemented the new payment model

--- src/react/Types/Types.ts
+++ src/react/Types/Types.ts
@@ -1,22 +1,61 @@
-import RuleCollection from "./RuleCollection";
+export type Amount = Balance;
 
-// an object filled with ruleCollections with the ruleCollectionId as a key
-export type RuleCollectionList = {
-    [key: string]: RuleCollection;
+export type AccountType =
+    | "MonetaryAccountLight"
+    | "MonetaryAccountBank"
+    | "MonetaryAccountJoint";
+
+export type Avatar = {
+    uuid: string;
+    anchor_uuid: string;
+    image: Image[];
 };
 
-// all valid rule types
-export type RuleTypes = "VALUE" | "TRANSACTION_AMOUNT" | "ITEM_TYPE";
+export type Address = {
+    street: string;
+    house_number: string;
+    po_box: string;
+    postal_code: string;
+    city: string;
+    country: string;
+    province: string;
+};
+
+export type AllCoOwnerItem = {
+    alias: ExtendedAlias[];
+    status: "ACCEPTED" | "REJECTED" | "PENDING" | "REVOKED";
+};
+
+export type AllCoOwner = AllCoOwnerItem[];
+
+export type Alias = {
+    type: "EMAIL" | "PHONE_NUMBER" | "IBAN";
+    value: string;
+    name: string;
+};
+
+export type Attachment = {
+    id: number;
+    monetary_account_id: number;
+};
+export type AttachmentList = Attachment[];
 
 export type Balance = {
     value: string;
     currency: "EUR";
 };
 
-export type Avatar = {
-    uuid: string;
-    anchor_uuid: string;
-    image: Image[];
+export type ExtendedAlias = Alias & {
+    public_nick_name: string;
+    display_name: string;
+    country: string;
+};
+
+export type Geolocation = {
+    latitude: number;
+    longitude: number;
+    altitude: number;
+    radius: number;
 };
 
 export type Image = {
@@ -26,14 +65,56 @@ export type Image = {
     width: number;
 };
 
-export type Alias = {
-    type: "EMAIL" | "PHONE_NUMBER" | "IBAN";
-    value: string;
-    name: string;
+export type MonetaryAccountSetting = {
+    color: string;
+    default_avatar_status:
+        | "AVATAR_DEFAULT"
+        | "AVATAR_CUSTOM"
+        | "AVATAR_UNDETERMINED";
+    restriction_chat: "ALLOW_INCOMING" | "BLOCK_INCOMING";
 };
 
-export type ExtendedAlias = Alias & {
-    public_nick_name: string;
+export type PaymentAlias = {
+    iban: string;
     display_name: string;
+    avatar: Avatar;
+    label_user: ExtendedAlias;
     country: string;
+    bunq_me: Alias;
+    is_light: boolean;
+    siwft_bic: null | string;
+    switft_account_number: null | string;
 };
+
+export type PaymentSubType =
+    | "PAYMENT"
+    | "WITHDRAWAL"
+    | "REVERSAL"
+    | "REQUEST"
+    | "BILLING"
+    | "SCT"
+    | "SDD"
+    | "NLO";
+
+export type PaymentType =
+    | "BUNQ"
+    | "EBA_SCT"
+    | "EBA_SDD"
+    | "IDEAL"
+    | "SWIFT"
+    | "FIS";
+
+export type RequestReferenceSplitTheBill = {
+    type: "RequestInquiry" | "RequestInquiryBatch";
+    id: number;
+};
+
+import RuleCollection from "./RuleCollection";
+
+// an object filled with ruleCollections with the ruleCollectionId as a key
+export type RuleCollectionList = {
+    [key: string]: RuleCollection;
+};
+
+// all valid rule types
+export type RuleTypes = "VALUE" | "TRANSACTION_AMOUNT" | "ITEM_TYPE";
