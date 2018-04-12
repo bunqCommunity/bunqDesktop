a61fca639a7f974b0e4a62e303f2af5f595129cb
Gregory Goijaerts <crecketgaming@gmail.com>
2018-04-12 13:42:20 +0200

    0.8.3

--- chocolatey/chocolateyInstall.ps1
+++ chocolatey/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $name = 'BunqDesktop'
 $installerType = 'exe'
-$url  = 'https://github.com/BunqCommunity/BunqDesktop/releases/download/0.8.2/BunqDesktop.Setup.0.8.2.exe'
+$url  = 'https://github.com/BunqCommunity/BunqDesktop/releases/download/0.8.3/BunqDesktop.Setup.0.8.3.exe'
 $silentArgs = '/silent'
 
 Install-ChocolateyPackage $name $installerType $silentArgs $url
