62c436254a92d7e4d2c8b9c43ec3446457ea3af8
Gregory Goijaerts <crecketgaming@gmail.com>
2018-05-29 18:56:21 +0200

    Added support for clearing refund status and changed refund color

--- src/react/Helpers/StatusTexts.js
+++ src/react/Helpers/StatusTexts.js
@@ -76,6 +76,10 @@ export const masterCardActionText = (masterCardAction, t) => {
             return `${t(
                 "The payment was blocked due to "
             )}${masterCardAction.decision_description}`;
+        case "CLEARING_REFUND":
+            return `${t(
+                "Payment was refunded due to "
+            )}${masterCardAction.decision_description}`;
         default:
             return `${t(
                 "The payment currently has the status "
@@ -84,21 +88,28 @@ export const masterCardActionText = (masterCardAction, t) => {
 };
 
 export const masterCardActionParser = (masterCardAction, t) => {
+    const defaultMessage = t("Card payment");
     const paymentText = t("Payment");
+    const refundText = t("Refund");
+
+    let secondaryText = paymentText;
+    if (masterCardAction.authorisation_status === "CLEARING_REFUND") {
+        secondaryText = refundText;
+    }
 
     if (masterCardAction.label_card) {
-        if(masterCardAction.wallet_provider_id === "103"){
-            return "Apple Pay " + paymentText;
+        if (masterCardAction.wallet_provider_id === "103") {
+            return "Apple Pay " + secondaryText;
         }
 
         switch (masterCardAction.label_card.type) {
             case "MAESTRO_MOBILE_NFC":
-                return "Tap & Pay " + paymentText;
+                return "Tap & Pay " + secondaryText;
             case "MASTERCARD":
-                return "Mastercard " + paymentText;
+                return "Mastercard " + secondaryText;
             case "MAESTRO":
-                return "Maestro " + paymentText;
+                return "Maestro " + secondaryText;
         }
     }
-    return "Card payment";
+    return defaultMessage;
 };
