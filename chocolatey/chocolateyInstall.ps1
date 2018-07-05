420e46ee81ec907c9db5f9b05c263aaca58c4743
Gregory Goijaerts <crecketgaming@gmail.com>
2018-07-05 16:26:15 +0200

    Upgrade to version 0.8.11

--- chocolatey/chocolateyInstall.ps1
+++ chocolatey/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $name = 'bunqDesktop'
 $installerType = 'exe'
-$url  = 'https://github.com/bunqCommunity/bunqDesktop/releases/download/0.8.10/bunqDesktop.Setup.0.8.10.exe'
+$url  = 'https://github.com/bunqCommunity/bunqDesktop/releases/download/0.8.11/bunqDesktop.Setup.0.8.11.exe'
 $silentArgs = '/silent'
 
 Install-ChocolateyPackage $name $installerType $silentArgs $url
