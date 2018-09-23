7191c38c017ab9241cb265355d0e1310b771bc4a
Gregory Goijaerts <crecketgaming@gmail.com>
2018-09-24 00:19:54 +0200

    Fix chocolatey download urls

--- chocolatey/chocolateyInstall.ps1
+++ chocolatey/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $name = 'bunqDesktop'
 $installerType = 'exe'
-$url  = 'https://github.com/bunqCommunity/bunqDesktop/releases/download/0.9.0/bunqDesktop.Setup.0.9.0.exe'
+$url  = 'https://github.com/bunqCommunity/bunqDesktop/releases/download/0.9.0/bunqDesktop-0.9.0.exe'
 $silentArgs = '/silent'
 
 Install-ChocolateyPackage $name $installerType $silentArgs $url
