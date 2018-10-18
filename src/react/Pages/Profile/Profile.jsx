d2e000e601bc86c2137166b91e2a4662d6777a90
Gregory Goijaerts <crecketgaming@gmail.com>
2018-10-18 20:59:35 +0200

    Remove unused import

--- src/react/Pages/Profile/Profile.jsx
+++ src/react/Pages/Profile/Profile.jsx
@@ -6,7 +6,6 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import Typography from "@material-ui/core/Typography";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
