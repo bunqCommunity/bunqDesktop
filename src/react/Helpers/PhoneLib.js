96ab14a8ad10ed3dae40c8419d01ff3401f38fa6
Gregory Goijaerts <crecketgaming@gmail.com>
2018-10-22 12:53:26 +0200

    Replace giant google libphonenumber with smaller/easier alternative

--- src/react/Helpers/PhoneLib.js
+++ src/react/Helpers/PhoneLib.js
@@ -1,26 +1,20 @@
-const PNF = require("google-libphonenumber").PhoneNumberFormat;
-const phoneUtil = require("google-libphonenumber").PhoneNumberUtil.getInstance();
+import { parsePhoneNumber } from "libphonenumber-js";
 
 export const isValidPhonenumber = (number, region = "NL") => {
     try {
-        const parsedNumber = phoneUtil.parseAndKeepRawInput(number, region);
-
-        return phoneUtil.isValidNumber(parsedNumber);
-    } catch (err) {}
-
+        const phoneNumber = parsePhoneNumber(number, region);
+        return phoneNumber.isValid();
+    } catch (error) {}
     return false;
 };
 
 export const getInternationalFormat = (number, region = "NL") => {
     try {
-        const parsedNumber = phoneUtil.parseAndKeepRawInput(number, region);
-
-        // format as international number
-        const formattedPhone = phoneUtil.format(parsedNumber, PNF.INTERNATIONAL);
+        const phoneNumber = parsePhoneNumber(number, region);
+        const formattedPhone = phoneNumber.formatInternational();
 
         // remove spaces and minus chars
         return formattedPhone.replace(/[\- ]/g, "");
     } catch (err) {}
-
     return false;
 };
