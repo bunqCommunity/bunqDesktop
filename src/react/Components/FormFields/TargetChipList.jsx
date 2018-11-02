32643118538ac3dd4220d9a72b778752a09cb6c7
Gregory Goijaerts <crecketgaming@gmail.com>
2018-11-02 21:31:20 +0100

    Add selection controls and improve checkbox handling

--- src/react/Components/FormFields/TargetChipList.jsx
+++ src/react/Components/FormFields/TargetChipList.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import Chip from "@material-ui/core/Chip";
-import Avatar from "@material-ui/core/Avatar";
 
-import CompareArrowsIcon from "@material-ui/icons/CompareArrows";
-import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
-import PersonIcon from "@material-ui/icons/Person";
+import TargetChip from "./TargetChip";
 
 const styles = {
     chips: {
@@ -12,44 +8,14 @@ const styles = {
     }
 };
 
-const TargetChipList = ({ targets, accounts, onClick = false, onDelete = false }) => {
+const TargetChipList = ({ targets, ...restProps }) => {
     return targets.map((target, targetKey) => {
-        let Icon = null;
-        let targetValue = target.value;
-        switch (target.type) {
-            case "EMAIL":
-            case "PHONE":
-            case "CONTACT":
-                Icon = PersonIcon;
-                break;
-            case "TRANSFER":
-                // for transfers we can try to display a description
-                if (accounts[target.value]) {
-                    targetValue = accounts[target.value].description;
-                }
-                Icon = CompareArrowsIcon;
-                break;
-            default:
-            case "IBAN":
-                Icon = AccountBalanceIcon;
-                break;
-        }
-
-        const chipProps = {};
-        if (onDelete) chipProps.onDelete = e => onDelete(targetKey);
-        if (onClick) chipProps.onDelete = e => onClick(targetKey);
-
         return (
-            <Chip
+            <TargetChip
                 key={targetKey}
-                label={targetValue}
-                style={styles.chips}
-                avatar={
-                    <Avatar>
-                        <Icon color="primary" />
-                    </Avatar>
-                }
-                {...chipProps}
+                target={target}
+                targetKey={targetKey}
+                {...restProps}
             />
         );
     });
--- src/react/Pages/PendingPayments/PendingPayments.jsx
+++ src/react/Pages/PendingPayments/PendingPayments.jsx
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 import { translate } from "react-i18next";
 import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
 import Paper from "@material-ui/core/Paper";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -13,25 +15,51 @@ import TableRow from "@material-ui/core/TableRow";
 import Checkbox from "@material-ui/core/Checkbox";
 import Typography from "@material-ui/core/Typography";
 
+import DeleteIcon from "@material-ui/icons/Delete";
+
 import LazyAttachmentImage from "../../Components/AttachmentImage/LazyAttachmentImage";
+import TranslateTypography from "../../Components/TranslationHelpers/Typography";
+import TargetChipList from "../../Components/FormFields/TargetChipList";
+import TargetChip from "../../Components/FormFields/TargetChip";
 
 import { formatMoney } from "../../Helpers/Utils";
 
 import {
+    // pendingPaymentsAddPayment,
+    // pendingPaymentsAddPayments,
     pendingPaymentsClear,
-    pendingPaymentsAddPayment,
-    pendingPaymentsAddPayments,
     pendingPaymentsClearAccount,
     pendingPaymentsRemovePayment
 } from "../../Actions/pending_payments";
 
 const styles = {
-    checkTableBoxCell: {
-        width: 72
+    paper: {
+        padding: 16
     },
-    accountTableCell: {
+    titleGrid: {
+        padding: 8
+    },
+    titleButtonGrid: {
+        textAlign: "right",
+        padding: 8
+    },
+    button: {
+        margin: 8
+    },
+    deleteIconTableCell: {
+        width: 48
+    },
+    amountTableCell: {
+        width: 100,
+        padding: "4px 24px 4px 24px"
+    },
+    accountTableCell: {},
+    accountWrapper: {
         display: "flex",
         alignItems: "center"
+    },
+    checkTableBoxCell: {
+        width: 48
     },
     avatar: {
         width: 40,
@@ -48,9 +76,29 @@ class PendingPayments extends React.Component {
         };
     }
 
-    togglePaymentCheckBox = checkBoxId => e => {
+    clearAll = () => {
+        this.props.pendingPaymentsClear();
+        this.setState({ selectedCheckBoxes: {} });
+    };
+    clearAccount = accountId => e => {
+        this.props.pendingPaymentsClearAccount(accountId);
+        this.setState({ selectedCheckBoxes: {} });
+    };
+    removePayment = paymendtId => e => {
+        this.props.pendingPaymentsRemovePayment(paymendtId);
+        this.setState({ selectedCheckBoxes: {} });
+    };
+
+    togglePaymentCheckBox = pendingPaymentId => e => {
+        const pendingPayments = this.props.pendingPayments;
         const selectedCheckBoxes = { ...this.state.selectedCheckBoxes };
-        selectedCheckBoxes[checkBoxId] = !selectedCheckBoxes[checkBoxId];
+        selectedCheckBoxes[pendingPaymentId] = !selectedCheckBoxes[pendingPaymentId];
+
+        if (!selectedCheckBoxes[pendingPaymentId]) {
+            selectedCheckBoxes[pendingPayments[pendingPaymentId].account_id] = false;
+            selectedCheckBoxes["all"] = false;
+        }
+
         this.setState({
             selectedCheckBoxes: selectedCheckBoxes
         });
@@ -61,6 +109,10 @@ class PendingPayments extends React.Component {
         const selectedCheckBoxes = { ...this.state.selectedCheckBoxes };
         selectedCheckBoxes[accountId] = !selectedCheckBoxes[accountId];
 
+        if (!selectedCheckBoxes[accountId]) {
+            selectedCheckBoxes["all"] = false;
+        }
+
         // filter payment items by the selected account id
         const accountMatchedPayments = Object.keys(pendingPayments).filter(pendingPaymentId => {
             return pendingPayments[pendingPaymentId].account_id === accountId;
@@ -68,14 +120,33 @@ class PendingPayments extends React.Component {
 
         // go through the matched types and set them to the same value as the account row
         accountMatchedPayments.forEach(accountMatchedPayment => {
-            const paymentCheckboxId = `payment-${accountMatchedPayment}`;
-            selectedCheckBoxes[paymentCheckboxId] = selectedCheckBoxes[accountId];
+            selectedCheckBoxes[accountMatchedPayment] = selectedCheckBoxes[accountId];
         });
 
         this.setState({
             selectedCheckBoxes: selectedCheckBoxes
         });
     };
+
+    toggleAllCheckbox = e => {
+        const pendingPayments = this.props.pendingPayments;
+        const selectedCheckBoxes = { ...this.state.selectedCheckBoxes };
+        selectedCheckBoxes["all"] = !selectedCheckBoxes["all"];
+
+        // go through the matched types and set them to the same value as the account row
+        Object.keys(pendingPayments).forEach(pendingPaymentId => {
+            selectedCheckBoxes[pendingPaymentId] = selectedCheckBoxes["all"];
+            selectedCheckBoxes[pendingPayments[pendingPaymentId].account_id] = selectedCheckBoxes["all"];
+        });
+
+        this.setState({
+            selectedCheckBoxes: selectedCheckBoxes
+        });
+    };
+
+    paySelected = () => {};
+
+    removeSelected = () => {};
 
     render() {
         const { t, BunqJSClient, accounts, pendingPayments } = this.props;
@@ -89,6 +160,10 @@ class PendingPayments extends React.Component {
             groupedPayments[pendingPayment.account_id].push(pendingPayment);
         });
 
+        const hasSelectedCheckboxes = Object.keys(this.state.selectedCheckBoxes).some(selectedCheckboxId => {
+            return this.state.selectedCheckBoxes[selectedCheckboxId];
+        });
+
         let componentList = [];
         Object.keys(groupedPayments).forEach(accountIdStr => {
             const accountId = parseFloat(accountIdStr);
@@ -98,22 +173,40 @@ class PendingPayments extends React.Component {
             if (!accountInfo) return;
             const imageUUID = accountInfo.avatar.image[0].attachment_public_uuid;
 
+            let accountSpentTotal = 0;
             const paymentItems = groupedPaymentList.map(pendingPayment => {
                 const paymentObject = pendingPayment.payment;
-                // console.log(pendingPayment);
+                accountSpentTotal = accountSpentTotal + parseFloat(paymentObject.amount.value);
 
-                const paymentCheckboxId = `payment-${pendingPayment.id}`;
+                let targetComponents = null;
+                if (paymentObject.counterparty_aliases) {
+                    targetComponents = (
+                        <TargetChipList targets={paymentObject.counterparty_aliases} accounts={this.props.accounts} />
+                    );
+                } else {
+                    targetComponents = (
+                        <TargetChip target={paymentObject.counterparty_alias} accounts={this.props.accounts} />
+                    );
+                }
+
                 return (
-                    <TableRow key={paymentCheckboxId}>
+                    <TableRow key={pendingPayment.id}>
                         <TableCell padding="checkbox" style={styles.checkTableBoxCell}>
                             <Checkbox
                                 color="primary"
-                                checked={!!this.state.selectedCheckBoxes[paymentCheckboxId]}
-                                onChange={this.togglePaymentCheckBox(paymentCheckboxId)}
+                                checked={!!this.state.selectedCheckBoxes[pendingPayment.id]}
+                                onChange={this.togglePaymentCheckBox(pendingPayment.id)}
                             />
                         </TableCell>
-                        <TableCell>Targets</TableCell>
-                        <TableCell>{formatMoney(paymentObject.amount.value)}</TableCell>
+                        <TableCell style={styles.amountTableCell}>
+                            <Typography>{formatMoney(paymentObject.amount.value)}</Typography>
+                        </TableCell>
+                        <TableCell>{targetComponents}</TableCell>
+                        <TableCell style={styles.deleteIconTableCell}>
+                            <IconButton onClick={this.removePayment(pendingPayment.id)}>
+                                <DeleteIcon />
+                            </IconButton>
+                        </TableCell>
                     </TableRow>
                 );
             });
@@ -129,23 +222,93 @@ class PendingPayments extends React.Component {
                                     onChange={this.toggleAccountCheckBox(accountId)}
                                 />
                             </TableCell>
-                            <TableCell style={styles.accountTableCell}>
-                                <Avatar style={styles.avatar}>
-                                    <LazyAttachmentImage
-                                        height={40}
-                                        BunqJSClient={BunqJSClient}
-                                        imageUUID={imageUUID}
-                                    />
-                                </Avatar>
-                                <Typography variant="body1">{accountInfo.description}</Typography>
+                            <TableCell style={styles.amountTableCell}>
+                                <Typography>Total:</Typography>
+                                <Typography>{formatMoney(accountSpentTotal)}</Typography>
                             </TableCell>
-                            <TableCell>{formatMoney(accountInfo.getBalance())}</TableCell>
+                            <TableCell style={styles.accountTableCell}>
+                                <div style={styles.accountWrapper}>
+                                    <Avatar style={styles.avatar}>
+                                        <LazyAttachmentImage
+                                            height={40}
+                                            BunqJSClient={BunqJSClient}
+                                            imageUUID={imageUUID}
+                                        />
+                                    </Avatar>
+                                    <div>
+                                        <Typography variant="body1">{accountInfo.description}</Typography>
+                                        <Typography variant="body2">{formatMoney(accountInfo.getBalance())}</Typography>
+                                    </div>
+                                </div>
+                            </TableCell>
+                            <TableCell style={styles.deleteIconTableCell}>
+                                <IconButton onClick={this.clearAccount(accountId)}>
+                                    <DeleteIcon />
+                                </IconButton>
+                            </TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>{paymentItems}</TableBody>
                 </React.Fragment>
             );
         });
+
+        const paperContent = (
+            <Grid container>
+                <Grid item xs={8} style={styles.titleGrid}>
+                    <TranslateTypography variant="h5">Pending payments</TranslateTypography>
+                </Grid>
+                <Grid item xs={4} style={styles.titleButtonGrid}>
+                    {componentList.length > 0 && (
+                        <Button color="secondary" variant="outlined" onClick={this.clearAll}>
+                            Clear all
+                        </Button>
+                    )}
+                </Grid>
+
+                {hasSelectedCheckboxes &&
+                    componentList.length > 0 && (
+                        <Grid item xs={12}>
+                            <Button
+                                style={styles.button}
+                                color="primary"
+                                variant="contained"
+                                onClick={this.paySelected}
+                            >
+                                Pay selected
+                            </Button>
+                            <Button
+                                style={styles.button}
+                                color="secondary"
+                                variant="contained"
+                                onClick={this.removeSelected}
+                            >
+                                Remove selected
+                            </Button>
+                        </Grid>
+                    )}
+
+                <Grid item xs={12}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell padding="checkbox" style={styles.checkTableBoxCell}>
+                                    <Checkbox
+                                        color="primary"
+                                        checked={!!this.state.selectedCheckBoxes["all"]}
+                                        onChange={this.toggleAllCheckbox}
+                                    />
+                                </TableCell>
+                                <TableCell>Amount</TableCell>
+                                <TableCell>Account / Targets</TableCell>
+                                <TableCell style={styles.deleteIconTableCell} />
+                            </TableRow>
+                        </TableHead>
+                        {componentList}
+                    </Table>
+                </Grid>
+            </Grid>
+        );
 
         return (
             <Grid container spacing={8}>
@@ -154,20 +317,7 @@ class PendingPayments extends React.Component {
                 </Helmet>
 
                 <Grid item xs={12}>
-                    <Paper>
-                        <Table>
-                            {/*<TableHead>*/}
-                            {/*<TableRow>*/}
-                            {/*<TableCell>Dessert (100g serving)</TableCell>*/}
-                            {/*<TableCell numeric>Calories</TableCell>*/}
-                            {/*<TableCell numeric>Fat (g)</TableCell>*/}
-                            {/*<TableCell numeric>Carbs (g)</TableCell>*/}
-                            {/*<TableCell>Protein (g)</TableCell>*/}
-                            {/*</TableRow>*/}
-                            {/*</TableHead>*/}
-                            {componentList}
-                        </Table>
-                    </Paper>
+                    <Paper style={styles.paper}>{paperContent}</Paper>
                 </Grid>
             </Grid>
         );
@@ -185,13 +335,13 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        pendingPaymentsRemovePayment: pendingPaymentId => dispatch(pendingPaymentsRemovePayment(pendingPaymentId)),
-        pendingPaymentsAddPayment: (accountId, pendingPayment) =>
-            dispatch(pendingPaymentsAddPayment(accountId, pendingPayment)),
-        pendingPaymentsAddPayments: (accountId, pendingPayments) =>
-            dispatch(pendingPaymentsAddPayments(accountId, pendingPayments)),
+        // pendingPaymentsAddPayment: (accountId, pendingPayment) =>
+        //     dispatch(pendingPaymentsAddPayment(accountId, pendingPayment)),
+        // pendingPaymentsAddPayments: (accountId, pendingPayments) =>
+        //     dispatch(pendingPaymentsAddPayments(accountId, pendingPayments)),
         pendingPaymentsClear: () => dispatch(pendingPaymentsClear()),
-        pendingPaymentsClearAccount: () => dispatch(pendingPaymentsClearAccount())
+        pendingPaymentsClearAccount: accountId => dispatch(pendingPaymentsClearAccount(accountId)),
+        pendingPaymentsRemovePayment: pendingPaymentId => dispatch(pendingPaymentsRemovePayment(pendingPaymentId))
     };
 };
 
