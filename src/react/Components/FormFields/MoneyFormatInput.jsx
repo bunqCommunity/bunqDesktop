391e99961b745d2b624a1dc10403b3e00d70eb53
Gregory Goijaerts <crecketgaming@gmail.com>
2019-04-28 12:02:01 +0200

    Upgrade react-number-format to allow . as decimal (fixes #512)

--- src/react/Components/FormFields/MoneyFormatInput.jsx
+++ src/react/Components/FormFields/MoneyFormatInput.jsx
@@ -1,33 +1,33 @@
 import React from "react";
 import NumberFormat from "react-number-format";
-import { preferedThousandSeparator, preferedDecimalSeparator } from "../../Functions/Utils";
-
 import { withTheme } from "@material-ui/core/styles";
 
-class MoneyFormatInput extends React.Component {
-    render() {
-        const { style, ...otherProps } = this.props;
+import { preferedThousandSeparator, preferedDecimalSeparator } from "../../Functions/Utils";
 
-        return (
-            <NumberFormat
-                min={0}
-                prefix="€ "
-                margin="normal"
-                decimalScale={2}
-                placeholder="€ 0.00"
-                className="money-input"
-                fixedDecimalScale={true}
-                style={{
-                    fontSize: 30,
-                    ...this.props.theme.styles.moneyInput,
-                    ...style
-                }}
-                decimalSeparator={preferedDecimalSeparator}
-                thousandSeparator={preferedThousandSeparator}
-                {...otherProps}
-            />
-        );
-    }
-}
+const MoneyFormatInput = props => {
+    const { style, ...otherProps } = props;
+
+    const mergedStyle = {
+        fontSize: 30,
+        ...props.theme.styles.moneyInput,
+        ...style
+    };
+
+    return (
+        <NumberFormat
+            min={0}
+            prefix="€ "
+            margin="normal"
+            decimalScale={2}
+            placeholder="€ 0.00"
+            className="money-input"
+            fixedDecimalScale={true}
+            style={mergedStyle}
+            decimalSeparator={preferedDecimalSeparator}
+            thousandSeparator={preferedThousandSeparator}
+            {...otherProps}
+        />
+    );
+};
 
 export default withTheme()(MoneyFormatInput);
